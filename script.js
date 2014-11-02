9757b61fae96ac353da53344bac6921b29b0ae76
Sam Corcos <sam@andaril.com>
2014-11-01 20:06:57 -0400

    solved the jquery issue

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     var userGuess = 0;
-    $("#submit").on("submit", function(){
+    $("#submit").on("click", function(){
+        console.log("internet")
         var input = +$(this).val();
         console.log(input)
     });
